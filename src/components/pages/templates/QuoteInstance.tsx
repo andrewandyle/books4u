330cc25a57c1a48fc8957b14ea4c0076ad4aaa5d
rahulramaswamy <rahul.ramaswamy@utexas.edu>
2021-03-03 22:09:48 -0500

    add quote dummy data

--- src/components/pages/templates/QuoteInstance.tsx
+++ src/components/pages/templates/QuoteInstance.tsx
@@ -1,8 +1,23 @@
 import React, { useEffect, useState } from "react";
 const quotes: Quote[] = [
-  { id: "1", text: "This is a quotation", author: "Author1", length: "10" },
-  { id: "2", text: "This is a quotation 2", author: "Author2", length: "20"},
-  { id: "3", text: "This is a quotation 3", author: "Author3", length: "10"},
+    { id: "0", 
+    text: "Be yourself; everyone else is already taken.", 
+    author: "Oscar Wilde", 
+    length: "7",
+    tags: "attributed-no-source, be-yourself, honesty, inspirational, misattributed-oscar-wilde",
+    image: "https://images.gr-assets.com/authors/1521044377p2/3565.jpg"},
+    { id: "1", 
+    text: "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.", 
+    author: "Marilyn Monroe", 
+    length: "41",
+    tags: "attributed-no-source, best, life, love, mistakes, out-of-control, truth, worst",
+    image: "https://images.gr-assets.com/quotes/1511992603p2/8630.jpg"},
+    { id: "2", 
+    text: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.", 
+    author: "Albert Einstein", 
+    length: "16",
+    tags: "attributed-no-source, human-nature, humor, infinity, philosophy, science, stupidity, universe13",
+    image: "https://images.gr-assets.com/authors/1429114964p2/9810.jpg"},
 ];
 
 interface Quote {
@@ -10,6 +25,8 @@ interface Quote {
   text: string;
   author: string;
   length: string;
+  tags: string;
+  image: string;
 }
 
 function QuoteInstance() {
@@ -18,18 +35,18 @@ function QuoteInstance() {
       const url = window.location.href;
       const split_url = url.split("/");
       const id = split_url[split_url.length - 1];
-      setQuoteId(parseInt(id) - 1);
+      setQuoteId(parseInt(id));
     }, []);
     return (
-      <div className="d-flex flex-column align-items-center">
-          <img className="p-2" src="../../../../src/logo.svg" alt="Quote Image"/>
-          <h4 className="p-2">{quotes[quoteId].text}</h4>
-          <p className="p-1">{quotes[quoteId].author}</p>
-          <p className="p-1">{quotes[quoteId].length}</p>
-          <p className="p-1">{quotes[quoteId].author}</p>
-          <p className="p-1">{quotes[quoteId].author}</p>        
-          <a href="#!" className="btn btn-primary">Link to Page?</a>
-      </div>
+        <div className="d-flex flex-column align-items-center">
+            <img className="p-2" src={quotes[quoteId].image} alt="Quote Image"/>
+            <h4 className="p-2">{quotes[quoteId].text}</h4>
+            <ul className="list-group">
+                <li className="list-group-item">Author: {quotes[quoteId].author}</li>
+                <li className="list-group-item">Length in Words: {quotes[quoteId].length}</li>
+                <li className="list-group-item">Tags: {quotes[quoteId].tags}</li>
+            </ul>     
+        </div>
     );
 }
 
