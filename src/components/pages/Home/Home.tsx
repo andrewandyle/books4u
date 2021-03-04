07e433ba02dd13c19433823c4d9e08f3aa3906fa
quddlr9015 <quddlr9015@gmail.com>
2021-03-03 21:14:44 -0600

    added image for homepage

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -9,12 +9,12 @@ function Home() {
       <h3>We're here to help you to find the books</h3>
     </div>
     
-    <div className = "container flex justify-content-center" style = {{height :600}}>
+    <div className = "container flex justify-content-center" style = {{height :"600"}}>
         <div className = "row">
           <div className = "column ml- mt-5 mb-5" style = {{width:"33%"}}>
             <div className="card text-center">
               <div className = "overflow">
-                <img src = "jsx-a11y/alt-text">
+                <img src = "https://media.wired.com/photos/5be4cd03db23f3775e466767/master/w_2560%2Cc_limit/books-521812297.jpg" alt = "bookImage" style={{height:310, width:"100%"}}>
                 </img>
               </div>
               <div className = "card-body text-dark">
@@ -30,7 +30,7 @@ function Home() {
           <div className = "column ml-5 mt-5 mb-5" style = {{width:"33%"}}>
             <div className="card text-center">
               <div className = "overflow">
-                <img src = "jsx-a11y/alt-text">
+                <img src = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Shakespeare_Droeshout_1623.jpg/900px-Shakespeare_Droeshout_1623.jpg" alt = "AuthorImage" style={{height:310, width:"100%"}}>
 
                 </img>
               </div>
@@ -47,7 +47,7 @@ function Home() {
           <div className = "column ml-5 mt-5 mb-5" style = {{width:"33%"}}>
             <div className="card text-center">
               <div className = "overflow">
-                <img src = "jsx-a11y/alt-text">
+                <img src = "https://libreshot.com/wp-content/uploads/2017/04/close-up-inside-of-a-book-973x691.jpg" alt = "QuotesImage" style={{height:310, width:"100%"}}>
 
                 </img>
               </div>
@@ -60,7 +60,7 @@ function Home() {
               </div>
             </div>
           </div>
-    
+
         </div>
       </div>
 
