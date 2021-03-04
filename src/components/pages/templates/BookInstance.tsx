cdc1e2b893fa2adea49fd59abc7d89a47db36ccf
rahulramaswamy <rahul.ramaswamy@utexas.edu>
2021-03-03 20:33:39 -0500

    stylize instance pages without data

--- src/components/pages/templates/BookInstance.tsx
+++ src/components/pages/templates/BookInstance.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-
 const books: Book[] = [
   { id: "1", title: "title1", author: "author1" },
   { id: "2", title: "title2", author: "author2" },
@@ -21,9 +20,14 @@ function BookInstance() {
     setBookId(parseInt(id) - 1);
   }, []);
   return (
-    <div>
-      Title: {books[bookId].title}
-      Author: {books[bookId].author}
+    <div className="d-flex flex-column align-items-center">
+        <img className="p-2" src="../../../../src/logo.svg" alt="Book Image"/>
+        <h2 className="p-2">{books[bookId].title}</h2>
+        <p className="p-1">{books[bookId].author}</p>
+        <p className="p-1">{books[bookId].author}</p>
+        <p className="p-1">{books[bookId].author}</p>
+        <p className="p-1">{books[bookId].author}</p>        
+        <a href="#!" className="btn btn-primary">Link to Page?</a>
     </div>
   );
 }
