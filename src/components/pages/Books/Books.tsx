c10c378ab6e8fd958da885789995f12c9c25570a
saikm200022 <saikiran.m00@gmail.com>
2021-03-03 20:49:21 -0600

    Add contents of dummy data

--- src/components/pages/Books/Books.tsx
+++ src/components/pages/Books/Books.tsx
@@ -4,9 +4,38 @@ import BookItem from "../../templates/Grid/items/BookItem";
 
 function Books() {
   const books = [
-    { id: "1", title: "title1", author: "author1" },
-    { id: "2", title: "title2", author: "author2" },
-    { id: "3", title: "title3", author: "author3" },
+    { id: "0", 
+      image: "http://books.google.com/books/content?id=_oG_iTxP1pIC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api", 
+      author: "Daniel Keyes",
+      title: "Flowers for Algernon",
+      genre: "Fiction",
+      year: "2007",
+      description: "Oscar-winning film Charly starring Cliff Robertson and Claire Bloom-a mentally challenged man receives an operation that turns him into a genius...and introduces him to heartache.",
+      pageCount: "304",
+      price: "8.99",
+      buyLink: "https://play.google.com/store/books/details?id=_oG_iTxP1pIC&rdid=book-_oG_iTxP1pIC&rdot=1&source=gbs_api"},
+  
+      { id: "1", 
+      image: "http://books.google.com/books/content?id=usvqAAAAMAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api", 
+      author: "Detroit Institute of Arts; George S. Keyes; Susan Donahue Kuretsky; Axel Rüger; Arthur K. Wheelock",
+      title: "Masters of Dutch Painting",
+      genre: "Art",
+      year: "2004",
+      description: "A comprehensive study of Dutch Art, from one of the world's premier collections.",
+      pageCount: "288",
+      price: "Not For Sale",
+      buyLink: "",},
+  
+      { id: "2", 
+      image: "http://books.google.com/books/content?id=jQxIAQAAMAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api", 
+      author: "Angela Mary Keyes",
+      title: "The Five Senses",
+      genre: "Senses and sensation",
+      year: "1911",
+      description: "No description found",
+      pageCount: "252",
+      price: "Not For Sale",
+      buyLink: "",}
   ];
   return (
     <div>
--- src/components/templates/Grid/items/BookItem.tsx
+++ src/components/templates/Grid/items/BookItem.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 function BookItem({ item }: any) {
-  const { id, title, author } = item;
+  const { id, author, title, year, price, pageCount} = item;
   return (
     <div className = "p-2 col-lg-4 col-md-6">
       <div className = "p-1 bg-light border border-dark rounded-lg container d-flex flex-column align-items-center"
             style = {{width : "75%", borderRadius: "0.3rem"}}>
-        <Link to={`/book/${id}`}>
-          title: {title}
+        <h3>{title}</h3>
+        <p style={{textAlign: "center"}}> 
+          Author: {author}
           <br></br>
-        </Link>
-        <p> author: {author}</p>
+          Year: {year}
+          <br></br>
+          Price: {price}
+          <br></br>
+          Page Count: {pageCount}
+        </p>
+        <a href={`/book/${id}`} className="btn btn-primary">See More</a>      
       </div>
     </div>
   );
