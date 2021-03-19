cfb8fcd590d77d3e8fcd88944bffd8406f7e3ffd
saikm200022 <saikiran.m00@gmail.com>
2021-03-18 19:13:26 -0500

    backend files from phase 1

---
+++ backend/deploy_backend.sh
@@ -0,0 +1,7 @@
+echo "Deploying Backend..."
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 416671831557.dkr.ecr.us-east-1.amazonaws.com/books4u-back:latest
+docker build -t books4u-back .
+docker tag books4u-back:latest 416671831557.dkr.ecr.us-east-1.amazonaws.com/books4u-back:latest
+docker push 416671831557.dkr.ecr.us-east-1.amazonaws.com/books4u-back:latest
+cd aws_deploy
+eb deploy
