42889de54f701f6e3a036f80e77ecf0dd7a60f74
saikm200022 <saikiran.m00@gmail.com>
2021-03-24 22:24:21 -0500

    Authors csv formatted better

--- backend/scrape.py
+++ backend/scrape.py
@@ -14,7 +14,7 @@ import pandas as pd
 # import models
 # import search
 
-def scrapeAwardAndBestSellerAuthors():
+def scrapeBestSellersAuthors():
     print("Enter function")
     author_params = {
         "rows" : "500",
@@ -33,9 +33,9 @@ def scrapeAwardAndBestSellerAuthors():
         "https://api.penguinrandomhouse.com/resources/v2/title/domains/PRH.US/authors/views/list-display", params=author_params
     ).json()
     return raw
-    dataframe = pd.DataFrame.from_dict(raw['data']['authors'])
 
-def scrapeAwardAndNotBestSellerAuthors():
+def scrapeNoBestsellersAuthors():
+    print("Enter second function")
     author_params = {
         "rows" : "500",
         "showAwards" : "true",
@@ -48,98 +48,114 @@ def scrapeAwardAndNotBestSellerAuthors():
         "api_key" : "d4huxgvevx94je6fp9eykxds"
     }
 
-
     raw = requests.get(
         "https://api.penguinrandomhouse.com/resources/v2/title/domains/PRH.US/authors/views/list-display", params=author_params
     ).json()
     return raw
-    dataframe = pd.DataFrame.from_dict(raw['data']['authors'])
-"""
-def scrapeNotAwardAndBestSellerAuthors():
-    author_params = {
-        "rows" : "10",
-        "showAwards" : "false",
-        "showBestSellers" : "false",
-        "showPublishedBooks" : "true",
-        "showCovers" : "true",
-        "hideAuthorsWithNoPhoto" : "true",
-        "showBio" : "true", 
-        "sort" : "random",
-        "api_key" : "d4huxgvevx94je6fp9eykxds"
-    }
-
-    raw = requests.get(
-        "https://api.penguinrandomhouse.com/resources/v2/title/domains/PRH.US/authors/views/list-display", params=author_params
-    ).json()
-"""
-
-#raw1 = scrapeAwardAndBestSellerAuthors()
-#raw2 = scrapeAwardAndNotBestSellerAuthors()
-#dataframe1 = pd.DataFrame.from_dict(raw1['data']['authors'])
-#dataframe2 = pd.DataFrame.from_dict(raw2['data']['authors'])
-#frames = [dataframe1, dataframe2]
-#authorsDataFrames = pd.concat(frames)
-#authorsDataFrames.to_csv('authors.csv', encoding = 'utf-8')
-
-authorsDataFrames = pd.read_csv('authors.csv')
-#print(authorsDataFrames['display'][0])
-
-def scrapeQuotesByAuthor(name):
-    quote_params = {
-        "author" : name,
-        #"minlength" : "0",
-        #"maxlength" : "300",
-        #"private" : "true",
-        "language" : "en",
-        "limit" : "1",
-        #"sfw" : "false",
-        "api_key" : "fB4po_6wvrjcPMLTOW_SKweF"
-    }
     
-    raw = requests.get(
-        "https://quotes.rest/quote/search", params=quote_params
-    ).json()
-    return raw
+authors_list = []
+result = scrapeBestSellersAuthors()
+for author in result['data']['authors']:   
+    authors_list.append({'first_name': author['first'], 'last_name' : author['last'], 'on_tour' : author['ontour'], 'bestsellers' : "True", 'spotlight' : author['spotlight'], 'image' : author['_links'][0]['href']})
 
-frames = []
-'''
-for i in range(1000):
-    result = scrapeQuotesByAuthor(authorsDataFrames['display'][i])
-    try:
-        dataframe = pd.DataFrame.from_dict(result['contents'])
-        frames.append(dataframe)
-    except:
-        continue
+result = scrapeNoBestsellersAuthors()
+for author in result['data']['authors']:   
+    authors_list.append({'first_name': author['first'], 'last_name' : author['last'], 'on_tour' : author['ontour'], 'bestsellers' : "False", 'spotlight' : author['spotlight'], 'image' : author['_links'][0]['href']})
+
+authorsDataFrames = pd.DataFrame.from_dict(authors_list)
+authorsDataFrames.to_csv('authors-fixed.csv', encoding = 'utf-8')
+
+
+#     dataframe = pd.DataFrame.from_dict(raw['data']['authors'])
+
+# def scrapeNotAwardAndBestSellerAuthors():
+#     author_params = {
+#         "rows" : "10",
+#         "showAwards" : "false",
+#         "showBestSellers" : "true",
+#         "showPublishedBooks" : "true",
+#         "showCovers" : "true",
+#         "hideAuthorsWithNoPhoto" : "true",
+#         "showBio" : "true", 
+#         "sort" : "random",
+#         "api_key" : "d4huxgvevx94je6fp9eykxds"
+#     }
+
+#     raw = requests.get(
+#         "https://api.penguinrandomhouse.com/resources/v2/title/domains/PRH.US/authors/views/list-display", params=author_params
+#     )
+
+#     print(raw)
+
+# scrapeNotAwardAndBestSellerAuthors()
+
+# #raw1 = scrapeAwardAndBestSellerAuthors()
+# #raw2 = scrapeAwardAndNotBestSellerAuthors()
+# #dataframe1 = pd.DataFrame.from_dict(raw1['data']['authors'])
+# #dataframe2 = pd.DataFrame.from_dict(raw2['data']['authors'])
+# #frames = [dataframe1, dataframe2]
+# #authorsDataFrames = pd.concat(frames)
+# #authorsDataFrames.to_csv('authors.csv', encoding = 'utf-8')
+
+# authorsDataFrames = pd.read_csv('authors.csv')
+# #print(authorsDataFrames['display'][0])
+
+# def scrapeQuotesByAuthor(name):
+#     quote_params = {
+#         "author" : name,
+#         #"minlength" : "0",
+#         #"maxlength" : "300",
+#         #"private" : "true",
+#         "language" : "en",
+#         "limit" : "1",
+#         #"sfw" : "false",
+#         "api_key" : "fB4po_6wvrjcPMLTOW_SKweF"
+#     }
     
-print(frames)
-quotesDataFrames = pd.concat(frames)
-quotesDataFrames.to_csv('quotes.csv', encoding = 'utf-8')
-'''
+#     raw = requests.get(
+#         "https://quotes.rest/quote/search", params=quote_params
+#     ).json()
+#     return raw
 
-#https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=yourAPIKey
+# frames = []
+# '''
+# for i in range(1000):
+#     result = scrapeQuotesByAuthor(authorsDataFrames['display'][i])
+#     try:
+#         dataframe = pd.DataFrame.from_dict(result['contents'])
+#         frames.append(dataframe)
+#     except:
+#         continue
+    
+# print(frames)
+# quotesDataFrames = pd.concat(frames)
+# quotesDataFrames.to_csv('quotes.csv', encoding = 'utf-8')
+# '''
 
-def scrapeBooksByAuthor(name):
-    book_params = {
-        "q" : '+inauthor:' + name,
-        "key" : "AIzaSyD9kP1HFu062nYfIm-tMqTaHWgnKHejZ7A"
+# #https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=yourAPIKey
+
+# def scrapeBooksByAuthor(name):
+#     book_params = {
+#         "q" : '+inauthor:' + name,
+#         "key" : "AIzaSyD9kP1HFu062nYfIm-tMqTaHWgnKHejZ7A"
         
-    }
+#     }
     
-    raw = requests.get(
-        "https://www.googleapis.com/books/v1/volumes", params=book_params
-    ).json()
-    return raw
+#     raw = requests.get(
+#         "https://www.googleapis.com/books/v1/volumes", params=book_params
+#     ).json()
+#     return raw
 
-quotesDataFrames = pd.read_csv('quotes.csv')
-frames = []
-for i in range(len(quotesDataFrames)):
-    result = scrapeBooksByAuthor(quotesDataFrames['requested_author'][i])
-    try:
-        dataframe = pd.DataFrame.from_dict(result['items'])
-        frames.append(dataframe)
-    except:
-        continue
+# quotesDataFrames = pd.read_csv('quotes.csv')
+# frames = []
+# for i in range(len(quotesDataFrames)):
+#     result = scrapeBooksByAuthor(quotesDataFrames['requested_author'][i])
+#     try:
+#         dataframe = pd.DataFrame.from_dict(result['items'])
+#         frames.append(dataframe)
+#     except:
+#         continue
 
-booksDataFrames = pd.concat(frames)
-booksDataFrames.to_csv('books.csv', encoding = 'utf-8')
+# booksDataFrames = pd.concat(frames)
+# booksDataFrames.to_csv('books.csv', encoding = 'utf-8')
 
