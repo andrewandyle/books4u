6c21dd2669f37373f7620f7057b4b795c3be3b4d
Andrew Le <andrewandyle@gmail.com>
2021-03-25 05:31:39 -0500

    remove all string exceptions

--- backend/scrape.py
+++ backend/scrape.py
@@ -141,7 +141,7 @@ def scrapeBooksByAuthor(name):
     book_params = {
         "q" : '+inauthor:' + name,
         "maxResults" : "40",
-        "key" : "AIzaSyD9kP1HFu062nYfIm-tMqTaHWgnKHejZ7A"
+        "key" : "AIzaSyCkpJRNS5r-1y-sdUpniuhgrzXdetkXFLU"
         
     }
     
@@ -155,7 +155,7 @@ def createBooksCsv():
     books_list = []
     for i in range(len(quotesDataFrames)):
         author_name = quotesDataFrames['author'][i]
-        print(author_name)
+        print(i, author_name)
         result = scrapeBooksByAuthor(quotesDataFrames['author'][i])
         try:
             result = result['items']
@@ -164,56 +164,61 @@ def createBooksCsv():
                 try:
                     curr['name'] = book['volumeInfo']['title']
                 except:
-                    curr['name'] = "No name available"
+                    curr['name'] = None
                 try:
                     curr['genres'] = book['volumeInfo']['categories']
                 except:
-                    curr['genres'] = "No genres available"
+                    curr['genres'] = None
                 try:
                     curr['year'] = book['volumeInfo']['publishedDate']
                 except:
-                    curr['year'] = "No year available"
+                    curr['year'] = None
                 try:
                     curr['page_count'] = book['volumeInfo']['pageCount']
                 except:
-                    curr['page_count'] = "No page count available"
+                    curr['page_count'] = None
                 try:
                     curr['purchase_link'] = book['saleInfo']['buyLink']
                     curr['price'] = book['saleInfo']['listPrice']['amount']
                 except:
-                    curr['purchase_link'] = "Not for sale"
-                    curr['price'] = "Not for sale"
+                    curr['purchase_link'] = None
+                    curr['price'] = None
                 try:
                     curr['avg_rating'] = book['volumeInfo']['averageRating']
                 except:
-                    curr['avg_rating'] = "No rating available"
+                    curr['avg_rating'] = None
                 try:
                     curr['num_ratings'] = book['volumeInfo']['ratingsCount']
                 except:
-                    curr['num_ratings'] = "No rating count"
+                    curr['num_ratings'] = None
                 try:
                     curr['maturity_rating'] = book['volumeInfo']['maturityRating']
                 except:
-                    curr['maturity_rating'] = "No maturity rating available"
+                    curr['maturity_rating'] = None
                 try:
                     curr['language'] = book['volumeInfo']['language']
                 except:
-                    curr['language'] = "No language available"
+                    curr['language'] = None
                 try:
                     curr['description'] = book['volumeInfo']['description']
                 except:
-                    curr['description'] = "No description available"
+                    curr['description'] = None
+                try:
+                    curr['image'] = book['volumeInfo']['imageLinks']['thumbnail']
+                except:
+                    curr['image'] = None
                 try:
                     authors_list = book['volumeInfo']['authors']
+                    # Check if current author has written current book
                     if author_name in authors_list:
                         curr['authors'] = book['volumeInfo']['authors']
                     else:
                         raise Exception("Author must match")
                 except:
+                    # Avoid adding current book if not written by current author
                     continue
                 books_list.append(curr)
         except:
-            print('higher level exception')
             continue
     booksDataFrames = pd.DataFrame.from_dict(books_list)
     booksDataFrames.to_csv('books-fixed.csv', encoding = 'utf-8')
