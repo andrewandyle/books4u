2e7b2a23a25338f75486a4fde8db296263d4c3cb
quddlr9015 <quddlr9015@gmail.com>
2021-03-24 23:17:45 -0500

    updated quotes-fixed

--- backend/scrape.py
+++ backend/scrape.py
@@ -97,38 +97,38 @@ def scrapeNoBestsellersAuthors():
 # #authorsDataFrames = pd.concat(frames)
 # #authorsDataFrames.to_csv('authors.csv', encoding = 'utf-8')
 
-authorsDataFrames = pd.read_csv('authors-fixed.csv')
+# authorsDataFrames = pd.read_csv('authors-fixed.csv')
 # #print(authorsDataFrames['display'][0])
 
-def scrapeQuotesByAuthor(name):
-    quote_params = {
-        "author" : name,
-        #"minlength" : "0",
-        #"maxlength" : "300",
-        #"private" : "true",
-        "language" : "en",
-        "limit" : "1",
-        #"sfw" : "false",
-        "api_key" : "fB4po_6wvrjcPMLTOW_SKweF"
-    }
+# def scrapeQuotesByAuthor(name):
+#     quote_params = {
+#         "author" : name,
+#         #"minlength" : "0",
+#         #"maxlength" : "300",
+#         #"private" : "true",
+#         "language" : "en",
+#         "limit" : "1",
+#         #"sfw" : "false",
+#         "api_key" : "fB4po_6wvrjcPMLTOW_SKweF"
+#     }
     
-    raw = requests.get(
-        "https://quotes.rest/quote/search", params=quote_params
-    ).json()
-    return raw
+#     raw = requests.get(
+#         "https://quotes.rest/quote/search", params=quote_params
+#     ).json()
+#     return raw
 
-quotes_list = []
+# quotes_list = []
 
-for i in range(len(authorsDataFrames)):
-    result = scrapeQuotesByAuthor(authorsDataFrames['full_name'][i])
-    try:
-        result = result['contents']['quotes']
-        quotes_list.append({'quote' : result[0]['quote'], 'length' : result[0]['length'], 'tags' : result[0]['tags'], 'category' : "None", 'language' : result[0]['language'], 'unique_words' : 0, 'num_syllables' : 0, 'score' : 0, 'most_common_word' : "", 'least_common_word' : "", 'link' : result[0]['permalink'], 'background' : result[0]['background']})
+# for i in range(len(authorsDataFrames)):
+#     result = scrapeQuotesByAuthor(authorsDataFrames['full_name'][i])
+#     try:
+#         result = result['contents']['quotes']
+#         quotes_list.append({'author' : authorsDataFrames['full_name'][i],'quote' : result[0]['quote'], 'length' : result[0]['length'], 'tags' : result[0]['tags'], 'category' : "None", 'language' : result[0]['language'], 'unique_words' : 0, 'num_syllables' : 0, 'score' : 0, 'most_common_word' : "", 'least_common_word' : "", 'link' : result[0]['permalink'], 'background' : result[0]['background']})
         
-    except:
-        continue
-quotesDataFrames = pd.DataFrame.from_dict(quotes_list)
-quotesDataFrames.to_csv('quotes-fixed.csv', encoding = 'utf-8')
+#     except:
+#         continue
+# quotesDataFrames = pd.DataFrame.from_dict(quotes_list)
+# quotesDataFrames.to_csv('quotes-fixed.csv', encoding = 'utf-8')
 
 # print(frames)
 # quotesDataFrames = pd.concat(frames)
@@ -137,27 +137,28 @@ quotesDataFrames.to_csv('quotes-fixed.csv', encoding = 'utf-8')
 
 # #https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=yourAPIKey
 
-# def scrapeBooksByAuthor(name):
-#     book_params = {
-#         "q" : '+inauthor:' + name,
-#         "key" : "AIzaSyD9kP1HFu062nYfIm-tMqTaHWgnKHejZ7A"
+def scrapeBooksByAuthor(name):
+    book_params = {
+        "q" : '+inauthor:' + name,
+        "key" : "AIzaSyD9kP1HFu062nYfIm-tMqTaHWgnKHejZ7A"
         
-#     }
+    }
     
-#     raw = requests.get(
-#         "https://www.googleapis.com/books/v1/volumes", params=book_params
-#     ).json()
-#     return raw
+    raw = requests.get(
+        "https://www.googleapis.com/books/v1/volumes", params=book_params
+    ).json()
+    return raw
 
-# quotesDataFrames = pd.read_csv('quotes.csv')
-# frames = []
-# for i in range(len(quotesDataFrames)):
-#     result = scrapeBooksByAuthor(quotesDataFrames['requested_author'][i])
-#     try:
-#         dataframe = pd.DataFrame.from_dict(result['items'])
-#         frames.append(dataframe)
-#     except:
-#         continue
+quotesDataFrames = pd.read_csv('quotes-fixed.csv')
+books_list = []
+for i in range(1):
+    result = scrapeBooksByAuthor(quotesDataFrames['author'][i])
+    print(result)
+    # try:
+    #     dataframe = pd.DataFrame.from_dict(result['items'])
+    #     books_list.append(dataframe)
+    # except:
+    #     continue
 
 # booksDataFrames = pd.concat(frames)
 # booksDataFrames.to_csv('books.csv', encoding = 'utf-8')
