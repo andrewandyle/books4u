b91556e4ea6a2af7efe17e931f2352eea9c1df9f
rahulramaswamy <rahul.ramaswamy@utexas.edu>
2021-03-28 16:43:52 -0500

    set to headless tests

--- frontend/guitests.py
+++ frontend/guitests.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 class tests(unittest.TestCase):
     def setUp(self):
         chrome_options = Options()
-        #chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument('--window-size=1920,1080')
         chrome_options.add_argument("--no-sandbox")
         self.driver = webdriver.Chrome(
