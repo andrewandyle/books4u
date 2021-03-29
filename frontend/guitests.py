b4f332d58de0b914fda7a8b8b960fdcac5495a38
rahulramaswamy <rahul.ramaswamy@utexas.edu>
2021-03-29 17:09:38 -0500

    add chrome workaround to gui tests

--- frontend/guitests.py
+++ frontend/guitests.py
@@ -11,6 +11,7 @@ class tests(unittest.TestCase):
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument('--window-size=1920,1080')
+        chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument("--no-sandbox")
         self.driver = webdriver.Chrome(
             "./node_modules/chromedriver/bin/chromedriver", options=chrome_options
