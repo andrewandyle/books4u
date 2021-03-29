78c7439829adf7fbf378c910eba6d083f48c7d6a
Fina Sierra <69916283+mjscs@users.noreply.github.com>
2021-03-28 22:42:59 -0500

    Books Model Page: Front Card Styling - Correction

--- frontend/src/components/templates/Grid/items/BookItem.tsx
+++ frontend/src/components/templates/Grid/items/BookItem.tsx
@@ -1,36 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ReactCardFlip from "react-card-flip";
+import "./BookItem.css";
+
 function BookItem({ item, excludeAuthor }: any) {
   const { id, authors, name, year, price, page_count, image } = item;
+  const [isFlipped, setIsFlipped] = React.useState(false);
   return (
     <div className="p-2 col-lg-4 col-md-6">
-      <div
-        className="p-1 bg-light border border-dark rounded-lg container d-flex flex-column align-items-center"
-        style={{ width: "75%", borderRadius: "0.3rem" }}
-      >
-        {image && <img src={image} alt={`Cover of ${name}`}></img>}
-        <h3>{name}</h3>
-        <p style={{ textAlign: "center" }}>
-          {!excludeAuthor && (
-            <div>
-              Author:{" "}
-              {authors
-                .map((author: any) => author.replace(/^'|'$/g, ""))
-                .join(", ")}
-              <br />
-            </div>
-          )}
-          Year: {year ? year.substring(0, 4) : "N/A"}
-          <br />
-          Price: {price ? `$${price}` : "N/A"}
-          <br />
-          Page Count: {page_count}
-        </p>
-        <Link to={`/book/${id}`} className="btn btn-primary">
-          See More
-        </Link>
-      </div>
-    </div>
+        <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+        <div onMouseEnter={() => setIsFlipped((prev) => !prev)} className="flip-card">
+            {image && <img className="image-container" src={image} alt={`Cover of ${name}`}></img>}
+            <h3 className="title">{name}</h3>
+          </div>
+          <div onMouseLeave={() => setIsFlipped((prev) => !prev)} className="flip-card">
+           <div className="flip-back">
+            This is back.
+           </div>
+           </div>
+        </ReactCardFlip>    
+    </div>  
+
   );
 }
 
