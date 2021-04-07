2e9dcdd704fb556ae1e0c2be676476c1d5245fdc
rahulramaswamy <rahul.ramaswamy@utexas.edu>
2021-04-07 17:29:32 -0500

    add python unit tests checking response data

--- backend/tests.py
+++ backend/tests.py
@@ -2,6 +2,9 @@ from app import app
 import unittest
 
 class FlaskTest(unittest.TestCase):
+    
+    # Books
+
     def test_books_status_code(self):
         tester = app.test_client(self)
         response = tester.get("/api/books")
@@ -12,25 +15,11 @@ class FlaskTest(unittest.TestCase):
         response = tester.get("/api/books")
         self.assertEqual(response.content_type, "application/json")
 
-    def test_authors_status_code(self):
+    def test_books_response_key(self):
         tester = app.test_client(self)
-        response = tester.get("/api/authors")
-        self.assertEqual(response.status_code, 200)
-
-    def test_authors_content_type(self):
-        tester = app.test_client(self)
-        response = tester.get("/api/authors")
-        self.assertEqual(response.content_type, "application/json")
-
-    def test_quotes_status_code(self):
-        tester = app.test_client(self)
-        response = tester.get("/api/quotes")
-        self.assertEqual(response.status_code, 200)
-
-    def test_quotes_content_type(self):
-        tester = app.test_client(self)
-        response = tester.get("/api/quotes")
-        self.assertEqual(response.content_type, "application/json")
+        response = tester.get("/api/books")
+        response_dict = response.json
+        self.assertEqual(set(response_dict), {'books'})
 
     def test_book_status_code(self):
         tester = app.test_client(self)
@@ -42,6 +31,30 @@ class FlaskTest(unittest.TestCase):
         response = tester.get("/api/book/0")
         self.assertEqual(response.content_type, "application/json")
 
+    def test_book_response_keys(self):
+        tester = app.test_client(self)
+        response = tester.get("/api/book/0")
+        response_dict = response.json
+        self.assertEqual(set(response_dict), {'book', 'related_authors', 'related_quotes'})
+
+    # Authors
+
+    def test_authors_status_code(self):
+        tester = app.test_client(self)
+        response = tester.get("/api/authors")
+        self.assertEqual(response.status_code, 200)
+
+    def test_authors_content_type(self):
+        tester = app.test_client(self)
+        response = tester.get("/api/authors")
+        self.assertEqual(response.content_type, "application/json")
+
+    def test_authors_response_key(self):
+        tester = app.test_client(self)
+        response = tester.get("/api/authors")
+        response_dict = response.json
+        self.assertEqual(set(response_dict), {'authors'})
+
     def test_author_status_code(self):
         tester = app.test_client(self)
         response = tester.get("/api/author/0")
@@ -52,6 +65,30 @@ class FlaskTest(unittest.TestCase):
         response = tester.get("/api/author/0")
         self.assertEqual(response.content_type, "application/json")
 
+    def test_author_response_keys(self):
+        tester = app.test_client(self)
+        response = tester.get("/api/author/0")
+        response_dict = response.json
+        self.assertEqual(set(response_dict), {'author', 'related_books', 'related_quotes'})
+
+    # Quotes
+
+    def test_quotes_status_code(self):
+        tester = app.test_client(self)
+        response = tester.get("/api/quotes")
+        self.assertEqual(response.status_code, 200)
+
+    def test_quotes_content_type(self):
+        tester = app.test_client(self)
+        response = tester.get("/api/quotes")
+        self.assertEqual(response.content_type, "application/json")
+
+    def test_quotes_response_key(self):
+        tester = app.test_client(self)
+        response = tester.get("/api/quotes")
+        response_dict = response.json
+        self.assertEqual(set(response_dict), {'quotes'})
+
     def test_quote_status_code(self):
         tester = app.test_client(self)
         response = tester.get("/api/quote/0")
@@ -61,6 +98,12 @@ class FlaskTest(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get("/api/quote/0")
         self.assertEqual(response.content_type, "application/json")
+
+    def test_quote_response_keys(self):
+        tester = app.test_client(self)
+        response = tester.get("/api/quote/0")
+        response_dict = response.json
+        self.assertEqual(set(response_dict), {'quote', 'related_author', 'related_books'})
 
 if __name__ == "__main__":
     unittest.main()
